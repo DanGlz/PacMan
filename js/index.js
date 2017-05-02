f1dac76746b73b1a4667e3e4773ee745913baf5b
dan gleyzer <dangl@post.bgu.ac.il>
2017-05-02 21:06:31 +0300

    ahooya

--- js/index.js
+++ js/index.js
@@ -2,37 +2,34 @@
  * Created by Dan gleyzer on 02-May-17.
  */
 
+var registered_users = {};
+var logged_user;
 
 
 
 
+function logClick() {
 
+    var username = document.getElementsByName("logName")[0].value;
+    var password = document.getElementsByName("logPassword")[0].value;
 
+    registered_users.a="a";
+   //  registered_users[dan]= "aaa";
 
+        if(registered_users[username]== password) {
+            alert("User: " + username + " logged in successfully!" + "\n\nEnjoy ! ")
+            logged_user = username;
+            openTab(event, 'PacMan');
+        }
+        else if(registered_users[username]===undefined){
+            alert("The user <"+username+"> doesn`t exist! Please register first. " )
+        }
+        else
+            alert("Wrong password entered! please try again." )
 
+    return;
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+}
 
 
 
