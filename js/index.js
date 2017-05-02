76c1dfdc0fe5eb0a60b5e91d353eee6578d237d1
dan gleyzer <dangl@post.bgu.ac.il>
2017-05-02 23:55:12 +0300

    ahooya

--- js/index.js
+++ js/index.js
@@ -5,9 +5,11 @@
 var registered_users = {};
 var logged_user;
 
+$(document).ready(function () {
+$('#Welcome').show();
+});
 
-
-
+// check the user name and password on log in form
 function logClick() {
 
     var username = document.getElementsByName("logName")[0].value;
@@ -28,9 +30,53 @@ function logClick() {
             alert("Wrong password entered! please try again." )
 
     return;
-
 }
 
+
+
+/*****About*****/
+
+
+$(document).mouseup(function (e)
+{
+
+// Get the modal
+    var modal = document.getElementById('myModal');
+
+// Get the element that opens the modal
+    var about = document.getElementById("About");
+
+// Get the <span> element that closes the modal
+    var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal
+    about.onclick = function() {
+        modal.style.display = "block";
+    }
+
+// When the user clicks on <span> (x), close the modal
+    span.onclick = function() {
+        modal.style.display = "none";
+    }
+
+// When the user clicks anywhere outside of the modal, close it
+    window.onclick = function(event) {
+        if (event.target == modal) {
+            modal.style.display = "none";
+        }
+    }
+});
+
+
+// draws welcome image when page opens
+window.onload = function() {
+    document.getElementById("det_footer").focus();
+    var c=document.getElementById('myCanvas');
+    var ctx=c.getContext("2d");
+    var img=document.getElementById('bkgdImg');
+    ctx.drawImage(img, 0, 0, img.width,img.height,     // source rectangle
+        0, 0, canvas.width, canvas.height);
+};
 
 
 /***Tabs***/
