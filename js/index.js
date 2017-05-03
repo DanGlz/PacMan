2f225d65ec031790eb835b134bb1a39add15a441
nitzan <nitzanmassad@gmail.com>
2017-05-03 14:07:36 +0300

    03.05.17

--- js/index.js
+++ js/index.js
@@ -4,6 +4,7 @@
 
 var registered_users = {};
 var logged_user;
+var idx = 0;
 
 $(document).ready(function () {
 $('#Welcome').show();
@@ -14,6 +15,7 @@ function logClick() {
 
     var username = document.getElementsByName("logName")[0].value;
     var password = document.getElementsByName("logPassword")[0].value;
+
 
     registered_users.a="a";
    //  registered_users[dan]= "aaa";
@@ -92,4 +94,54 @@ function openTab(evt, divName) {
     }
     document.getElementById(divName).style.display = "block";
     evt.currentTarget.className += " active";
+}
+function regClick() {
+    var username = document.getElementsByName('regName')[0].value;
+    var pswd = document.getElementsByName('regPassword')[0].value;
+    var confPswd = document.getElementsByName('conf_pswd')[0].value;
+    var fname = document.getElementsByName('fname')[0].value;
+    var lname = document.getElementsByName('lname')[0].value;
+    var email = document.getElementsByName('email')[0].value;
+    var dob = document.getElementsByName('dob')[0].value;
+
+    if (username.length < 1 || !username.trim()){
+        alert("User name should be at least 1 char!");
+    }
+    else if (pswd.length < 8 || !(pswd.match(/\d+/g)) || !(pswd.match(/[a-z]/i))){
+        alert("Password Length should be 8 characters and should include numbers and letters!")
+    }
+    else if (confPswd != pswd){
+        alert("Password confirmation doesn't match first password!")
+    }
+    else if ((fname.length < 1) || (fname.match(/\d+/g))){
+        alert("First name length should be larger then 1, without numbers!");
+    }
+    else if ((lname.length < 1) || (lname.match(/\d+/g))){
+        alert("Last name length should be larger then 1, without numbers!");
+    }
+    else if (!email.includes("@") || !email.includes(".")){
+        alert("Email is invalid!");
+    }
+    else if (!dob.match(/\d+/g) || dob.match(/[a-z]/i)){
+        alert("Date of birth is invalid!")
+    }
+    else{
+        registered_users[idx] = {};
+       // registered_users[idx, 0] = username;
+        //registered_users[idx, 1] = pswd;
+        registered_users[username]=pswd ;
+        idx++;
+        alert("User registered successfully!");
+        document.getElementsByName('regName')[0].value = "";
+        document.getElementsByName('regPassword')[0].value = "";
+        document.getElementsByName('conf_pswd')[0].value = "";
+        document.getElementsByName('fname')[0].value = "";
+        document.getElementsByName('lname')[0].value = "";
+        document.getElementsByName('email')[0].value = "example@example.com";
+        document.getElementsByName('dob')[0].value = "";
+        document.getElementsByName("logName")[0].value = "";
+        document.getElementsByName("logPassword")[0].value = "";
+        document.getElementById("reg").style.display = "none";
+        document.getElementById("log").style.display = "block";
+    }
 }
