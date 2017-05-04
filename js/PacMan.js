2ba577f1599b16f95760f25ebcf7c8f0f7b099be
nitzan <nitzanmassad@gmail.com>
2017-05-04 18:12:41 +0300

    04.05.17

--- js/PacMan.js
+++ js/PacMan.js
@@ -30,8 +30,8 @@ ball_15points= new Image() ;
 ball_15points.src="images/15points.png";
 ball_25points= new Image() ;
 ball_25points.src="images/25points.png";
-BonusItemImage= new Image() ;
-BonusItemImage.src="images/bonusItem.jpeg";
+//BonusItemImage= new Image() ;
+//BonusItemImage.src="images/bonusItem.jpeg";
 
 function Start() {
     board = new Array();
@@ -46,7 +46,7 @@ function Start() {
     start_time= new Date();
     BonusItem.i = 14 ;
     BonusItem.j = 10 ;
-    chooseRandomSpotForBonusItem() ;
+   // chooseRandomSpotForBonusItem() ;
     for (var i = 0; i < 15; i++) { // columns
         board[i] = new Array();
         //put obstacles in (i=3,j=3) and (i=3,j=4) and (i=3,j=5), (i=6,j=1) and (i=6,j=2)
@@ -260,15 +260,16 @@ function Draw() {
             else if (boardOfMonsters[i][j]==3 ){
                 context.drawImage(monster3,center.x-20, center.y-20,40,40);
             }
-            context.drawImage(BonusItemImage,i * 40 + 20-20, j * 40 + 20-20,40,40);
+            //context.drawImage(BonusItemImage,i * 40 + 20-20, j * 40 + 20-20,40,40);
 
         }
     }
     counter ++ ;
-    if (counter%10 == 0)
+/*    if (counter%10 == 0)
     {
         moveBonusItem() ;
     }
+    */
 }
 function UpdatePosition() {
     board[shape.i][shape.j]=0;
@@ -360,11 +361,12 @@ function randomBalls ()
     }
     }
 }
-function moveBonusItem ()
+/*function moveBonusItem ()
 {
 
  }
-function chooseRandomSpotForBonusItem ()
+ */
+/*function chooseRandomSpotForBonusItem ()
 {
       var i = Math.floor((Math.random() * 14) + 1);
       var j = Math.floor((Math.random() * 10) + 1);
@@ -376,5 +378,5 @@ function chooseRandomSpotForBonusItem ()
        BonusItem.next_i =i ;
        BonusItem.next_j =j ;
 }
-
+*/
 window.addEventListener("load", Start, false);
